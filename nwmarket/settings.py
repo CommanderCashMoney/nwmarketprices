765d7d159ace7417b1662c6c9d8ff837664d8aec
J <valesyde@gmail.com>
2022-01-12 17:06:44 -0800

    initial

--- nwmarket/settings.py
+++ nwmarket/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "nwmarketapp", "static")
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
