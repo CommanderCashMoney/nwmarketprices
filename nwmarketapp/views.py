9ecf78e803c93e9b1cf169ad39e5ca973109f2ba
J <valesyde@gmail.com>
2022-01-23 21:48:23 -0800

    update cache time to 15mins

--- nwmarketapp/views.py
+++ nwmarketapp/views.py
@@ -67,7 +67,7 @@ def get_list_by_nameid(name_id):
 
     return grouped_hist, recent_lowest_price, price_change, price_change_text
 
-@cache_page(60 * 120)
+@cache_page(60 * 15)
 def index(request):
     confirmed_names = ConfirmedNames.objects.all().exclude(name__contains='"')
     confirmed_names = confirmed_names.values_list('name', 'id')
