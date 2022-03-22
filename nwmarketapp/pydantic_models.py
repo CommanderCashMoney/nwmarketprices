327b89d636e1464907e0ff91c592171eafb9a7da
Jack <jackward84@gmail.com>
2022-03-22 13:22:50 +1300

    clean up

--- nwmarketapp/pydantic_models.py
+++ nwmarketapp/pydantic_models.py
@@ -1,18 +1,25 @@
 from datetime import datetime
 from decimal import Decimal
-from typing import List
+from typing import Dict, List, Union
 
-from pydantic import BaseModel
+from pydantic import BaseModel, root_validator
+
+from nwmarketapp.models import Prices
 
 
 class ItemPriceHistory(BaseModel):
     scan_time: datetime
     price: Decimal
-    quantity: int
+
+    @root_validator
+    def validate(cls, price_obj: Union[Dict, Prices]):
+        if isinstance(price_obj, dict):
+            return price_obj
+        return cls(scan_time=price_obj.timestamp, price=price_obj.price)
 
 
 class ItemSummary(BaseModel):
-    grouped_hist: List[ItemPriceHistory]
+    grouped_hist: List[List[ItemPriceHistory]]
     recent_lowest_price: Decimal
     price_change: Decimal
     price_change_text: str  # todo: property
