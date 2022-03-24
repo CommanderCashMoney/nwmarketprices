54c2fed4735f3a7ad11d965f458fbda781d06ae9
Jack <jackward84@gmail.com>
2022-03-24 22:34:22 +1300

    working migration

--- nwmarketapp/migrations/0004_migrate_prices_add_run_id.py
+++ nwmarketapp/migrations/0004_migrate_prices_add_run_id.py
@@ -4,34 +4,40 @@ import django
 from django.db import connection, migrations, models
 from django.db.migrations.state import StateApps
 
+FAKE_RUN_USER = "january"
+
 
 def update_run_ids_for_ts_range(run_id: int, obj_dict: dict) -> None:
     start_date = obj_dict["start_date"].isoformat().replace("T", " ")
     end_date = obj_dict["end_date"].isoformat().replace("T", " ")
     server_id = obj_dict["server_id"]
-    approved = int(obj_dict["approved"])
+    approved = obj_dict["approved"]
+    query = f"""
+    UPDATE prices
+    SET run_id={run_id}
+    WHERE
+        server_id={server_id} AND
+        timestamp BETWEEN '{start_date}' AND '{end_date}' AND
+        approved={approved}
+    """
+    print(f"Updating run id {run_id}")
+    print(query)
     with connection.cursor() as cursor:
-        cursor.execute(f"""
-        UPDATE prices
-        SET run_id={run_id}
-        WHERE
-            server_id={server_id} AND
-            timestamp BETWEEN '{start_date}' AND '{end_date}' AND
-            approved={approved}
-        """)
-    print(f"Updated run id {run_id}")
-
-
-def delete_prices_before_feb():
-    with connection.cursor() as cursor:
-        cursor.execute("DELETE FROM prices WHERE timestamp < '2022-02-01';")
+        cursor.execute(query)
+    print(f"Done\n===================================")
 
 
 def forwards(apps: StateApps, schema_editor):
     Run = apps.get_model("nwmarketapp", "Run")  # noqa
     Prices = apps.get_model("nwmarketapp", "Price")  # noqa
+
+    uther_run_mismatched_approve = Run.objects.get(id=199)
+    uther_run_mismatched_approve.approved = False
+    uther_run_mismatched_approve.save()
+
     unique_server_ids = set([value[0] for value in Run.objects.all().values_list("server_id").distinct()])
     run_timestamp_ranges = {}
+
     for server_id in unique_server_ids:
         all_runs_for_server = Run.objects.filter(server_id=server_id).order_by("id")
         last_server_run_id: int = None
@@ -47,18 +53,36 @@ def forwards(apps: StateApps, schema_editor):
                 "approved": run.approved
             }
             if last_server_run_id is not None:
-                server_timestamp_ranges[last_server_run_id]["end_date"] = run.start_date - datetime.timedelta(seconds=1)
+                one_second_before_this_run = run.start_date - datetime.timedelta(seconds=1)
+                server_timestamp_ranges[last_server_run_id]["end_date"] = one_second_before_this_run
+                id_from_2_runs_ago = server_timestamp_ranges[last_server_run_id]["previous_run_id"]
+                # super hack to handle cases where there were overlapping runs
+                unapproved_overlapping_runs = [117, 133, 129]
+                if last_server_run_id in unapproved_overlapping_runs:
+                    server_timestamp_ranges[id_from_2_runs_ago]["end_date"] = one_second_before_this_run
+
             last_server_run_id = run.id
+
+    fake_run = Run(server_id=1, approved=False, username=FAKE_RUN_USER, start_date=datetime.datetime.utcnow())
+    fake_run.save()
+
+    with connection.cursor() as cursor:
+        cursor.execute(f"""
+            UPDATE prices
+            SET run_id={fake_run.id}
+            WHERE timestamp < '2022-02-01'
+        """)
 
     for server_id, runs in run_timestamp_ranges.items():
         for run_id, run in runs.items():
             update_run_ids_for_ts_range(run_id, run)
 
-    delete_prices_before_feb()
+    # if we reach the end of the script with no errors, all price:run data has integrity!
 
 
 def backwards(apps: StateApps, schema_editor):
-    pass  # no point
+    Run = apps.get_model("nwmarketapp", "Run")  # noqa
+    Run.objects.filter(username=FAKE_RUN_USER).delete()
 
 
 class Migration(migrations.Migration):
@@ -71,6 +95,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='price',
             name='run',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='nwmarketapp.run'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='nwmarketapp.run', null=True),
         ),
     ]
