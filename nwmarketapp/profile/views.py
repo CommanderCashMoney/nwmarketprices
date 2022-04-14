47043ebfc31b367502631a7ecb05f8e361833b81
Jack <jackward84@gmail.com>
2022-04-14 19:19:47 +1200

    redirect non logged in users

--- nwmarketapp/profile/views.py
+++ nwmarketapp/profile/views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.password_validation import get_default_password_validators, validate_password
 from django.core.exceptions import ValidationError
 from django.core.handlers.wsgi import WSGIRequest
@@ -8,6 +9,7 @@ from django.http import JsonResponse
 from django.template.response import TemplateResponse
 
 
+@login_required(login_url="/", redirect_field_name="")
 def profile(request: WSGIRequest) -> TemplateResponse:
     return TemplateResponse(request, "profile/index.html")
 
