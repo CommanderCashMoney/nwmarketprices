a84d35362730c37e9acdcb364f65dc86de6815af
Jason <valesyde@gmail.com>
2022-05-16 10:55:14 -0700

    Add CORS whitelist for gaming.tools

--- nwmarket/settings.py
+++ nwmarket/settings.py
@@ -52,9 +52,11 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.discord',
     'nwmarketapp.apps.NwmarketappConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,7 +64,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
 ]
+CORS_ALLOWED_ORIGINS = [
+    'https://local.gaming.tools',
+    'https://gaming.tools',
+]
+
 
 ROOT_URLCONF = 'nwmarket.urls'
 
