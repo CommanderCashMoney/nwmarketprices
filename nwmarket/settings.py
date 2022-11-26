30ff398fa497c3b1c3eee8a8e2e7f1d3d2e81054
Jason <valesyde@gmail.com>
2022-11-25 18:52:17 -0800

    Add new host url

--- nwmarket/settings.py
+++ nwmarket/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-HOST = os.getenv("HOST", "nwmp.eba-ubwpcedf.ap-southeast-2.elasticbeanstalk.com")
+HOST = os.getenv("HOST", "nwmarket-env-2.eba-rxcymaas.us-west-1.elasticbeanstalk.com/")
 ALLOWED_HOSTS = [HOST, "127.0.0.1", 'localhost', 'nwmarketprices.com']
 
 
