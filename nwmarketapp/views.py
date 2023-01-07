b04a19c09cc6e8d7915e844accbe9cd8f769b96d
Jason <valesyde@gmail.com>
2023-01-07 11:31:02 -0800

    Add timezone convert to server updated time

--- nwmarketapp/views.py
+++ nwmarketapp/views.py
@@ -1,6 +1,7 @@
 import logging
 from typing import List, Tuple
 import pytz
+import tzlocal
 import requests
 from constance import config  # noqa
 from datetime import datetime
@@ -284,7 +285,9 @@ def get_serverlist():
         if item[2] and item[3]:
             tz = pytz.timezone(item[3])
             last_scan_utc = tz.normalize(tz.localize(item[2])).astimezone(pytz.utc)
-
+            localtz = tzlocal.get_localzone()
+            localtime = last_scan_utc.replace(tzinfo=pytz.utc).astimezone(localtz)
+            localtime = localtime.replace(tzinfo=None)
             last_scan_utc = last_scan_utc.replace(tzinfo=None)
             timediff = current_utc_time - last_scan_utc
             hours_since_last_scan = timediff.total_seconds() / 3600
@@ -295,7 +298,7 @@ def get_serverlist():
             else:
                 dot_color = 'green-dot'
 
-            server_details[item[0]] = {'name': item[1], 'health': dot_color, 'last_scanned': humanize.naturaltime(item[2])}
+            server_details[item[0]] = {'name': item[1], 'health': dot_color, 'last_scanned': humanize.naturaltime(localtime)}
         else:
             server_details[item[0]] = {'name': item[1], 'health': 'red-dot', 'last_scanned': None}
     return server_details
