a183b5c00d1337d8aa4accad62d2e42b513e6caf
Jason <valesyde@gmail.com>
2023-01-12 07:45:24 -0800

    fresh deploy. no changes

--- nwmarketapp/management/commands/nwmp_rolemonitor_bot.py
+++ nwmarketapp/management/commands/nwmp_rolemonitor_bot.py
@@ -148,4 +148,4 @@ class Command(BaseCommand):
 # $ sudo su -
 # $ export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
 # $ source /var/app/venv/*/bin/activate
-# $ python3 /var/app/current/manage.py nwmp_rolemonitor_bot
+# $
