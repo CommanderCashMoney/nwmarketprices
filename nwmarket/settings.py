b1eddb1442147a1da8c6457bff2b7f0ca5c95338
Jason <valesyde@gmail.com>
2023-01-30 13:36:14 -0800

    switching to new env

--- nwmarket/settings.py
+++ nwmarket/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = os.getenv("DEBUG") == "True"
 
 HOST = os.getenv("HOST", "nwmarket-env-1.eba-rxcymaas.us-west-1.elasticbeanstalk.com")
-# ALLOWED_HOSTS = [HOST, "127.0.0.1", 'localhost', 'nwmarketprices.com']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [HOST, "127.0.0.1", 'localhost', 'nwmarketprices.com', "nwmarket-env-1.eba-rxcymaas.us-west-1.elasticbeanstalk.com"]
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
