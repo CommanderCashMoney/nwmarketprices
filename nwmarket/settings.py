7854e17829202f669dfece4bf708dd3882179154
Jason <valesyde@gmail.com>
2023-09-21 18:55:07 -0700

    new environment

--- nwmarket/settings.py
+++ nwmarket/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-HOST = os.getenv("HOST", "nwmarket-env-5.eba-rxcymaas.us-west-1.elasticbeanstalk.com")
-ALLOWED_HOSTS = [HOST, "127.0.0.1", 'localhost', 'nwmarketprices.com', "nwmarket-env-5.eba-rxcymaas.us-west-1.elasticbeanstalk.com"]
+HOST = os.getenv("HOST", "nwmarket-env-1-env.eba-ed2yi8au.us-west-1.elasticbeanstalk.com/")
+ALLOWED_HOSTS = [HOST, "127.0.0.1", 'localhost', 'nwmarketprices.com', "nwmarket-env-1-env.eba-ed2yi8au.us-west-1.elasticbeanstalk.com/"]
 # ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = 'ALLOW-FROM https://aeternum-map.gg/'
 # Application definition
