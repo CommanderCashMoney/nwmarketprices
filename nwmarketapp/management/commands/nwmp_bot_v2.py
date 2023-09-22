73eba1a4d2804637ef66b3e73c1450ca451ff864
Jason <valesyde@gmail.com>
2023-09-22 09:06:12 -0700

    Update discord bot

--- nwmarketapp/management/commands/nwmp_bot_v2.py
+++ nwmarketapp/management/commands/nwmp_bot_v2.py
@@ -13,7 +13,7 @@ curr = conn.cursor()
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='/var/log/discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
